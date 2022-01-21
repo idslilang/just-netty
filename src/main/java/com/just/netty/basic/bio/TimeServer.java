b3d84a6f0352c013d34faf78ac5ea3664a0b3972
idslilang <1170370113@qq.comgit>
2022-01-22 07:22:50 +0800

    阻塞IO

--- src/main/java/com/just/netty/basic/bio/TimeServer.java
+++ src/main/java/com/just/netty/basic/bio/TimeServer.java
@@ -1,8 +1,11 @@
 package com.just.netty.basic.bio;
 
+import cn.hutool.core.thread.ExecutorBuilder;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
 
 /**
  * @description:
@@ -14,6 +17,11 @@ public class TimeServer {
 
     public static void main(String[] args) {
 
+      startBio();
+    }
+
+    //阻塞IO
+    public static void startBio(){
         int port = 8090;
         ServerSocket serverSocket = null;
         try {
@@ -26,6 +34,49 @@ public class TimeServer {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            try {
+                if (serverSocket != null) {
+                    serverSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
+
+    /**
+     * 当服务端处理速度比较慢的时候，并发连接将会导致大量超时
+     */
+    public static void  startAbio(){
+        ExecutorService executor = ExecutorBuilder.create()//
+                .setCorePoolSize(5)//
+                .setMaxPoolSize(10)//
+                .setKeepAliveTime(10)//
+                .build();
+        int port = 8091;
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("server start in port " + port);
+            Socket socket = null;
+            while (true) {
+                socket = serverSocket.accept();
+                executor.submit(new TimeServerHandler(socket));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (serverSocket != null) {
+                    serverSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            executor.shutdown();
+        }
+    }
+
 }
